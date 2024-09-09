07c00a9a065683ed1947d95aa506f922b6b3c4c4
BrandonMW235 <160472385+BrandonMW235@users.noreply.github.com>
2024-09-10 03:44:12 +1000

    Update OpalCard.java

--- src/main/java/uts/isd/model/model/OpalCard.java
+++ src/main/java/uts/isd/model/model/OpalCard.java
@@ -1,1 +1,34 @@
+package uts.isd.model;
 
+public class OpalCard
+{
+  private String cardNumber;
+  private double balance;
+
+  public OpalCard (String cardNumber, double balance)
+  {
+    this.cardNumber = cardNumber;
+    this.balance = balance;
+  }
+
+  public String getCardNumber()
+  {
+    return cardNumber;
+  }
+
+  public void setCardNumber(String cardNumber)
+  {
+    this.cardNumber = cardNumber;
+  }
+
+  public double getBalance()
+  {
+    return balance;
+  }
+
+  public void setBalance()
+  {
+    this.balance = balance;
+  }
+}
+    
