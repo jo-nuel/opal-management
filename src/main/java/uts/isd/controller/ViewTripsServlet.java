1ac525bfda42c807e4078869bca27b6b320c2cd8
BrandonMW235 <160472385+BrandonMW235@users.noreply.github.com>
2024-10-27 21:08:13 +1100

    Update ViewTripsServlet.java

--- src/main/java/uts/isd/controller/ViewTripsServlet.java
+++ src/main/java/uts/isd/controller/ViewTripsServlet.java
@@ -5,20 +5,35 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import uts.isd.model.*;
-import uts.isd.model.dao.*;
+import uts.isd.dao.*;
+import java.util.List;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet("/viewTrips")
 public class ViewTripsServlet extends HttpServlet {
+    private TripDAO tripDAO;
+
+
+    public void init() {
+        Connection conn = (Connection) getServletContext().getAttribute("conn");
+        if (conn != null) 
+        {
+            tripDAO = new TripDAO(conn);
+        }
+    }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         String userID = (String) session.getAttribute("userID");
 
+
         try {
-            TripDAO tripDAO = new TripDAO(DBConnection.getConnection());
             List<Trip> trips = tripDAO.getTripsByUser(userID);
             request.setAttribute("trips", trips);
             request.getRequestDispatcher("tripSummary.jsp").forward(request, response);
-        } catch (Exception e) {
+        } catch (Exception e) 
+        {
             throw new ServletException("Error viewing trips", e);
         }
     }
